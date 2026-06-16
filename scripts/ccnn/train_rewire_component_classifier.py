fix issues with weighting

scripts/ccnn/train_rewire_component_classifier.py
@@ -1,3 +1,5 @@
+import torch
+import os.path as osp
 
 from scripts.CONFIG import CONFIG
 from ticllearning.cclinking.component_classifier import CellClassifier
@@ -7,6 +9,7 @@ from ticllearning.cclinking.train import train_model
 if __name__ == "__main__":
     data_info = "closeby_multi_0pu"
     experiment_name = "laplacian_build_cell_linking"
+    retrain = True
 
     in_channels = [6, 16, 16]
     hidden_channels = 32
@@ -22,4 +25,14 @@ if __name__ == "__main__":
         attention=True
     ).to(CONFIG.device)
 
-    train_model(model, data_info, experiment_name, CONFIG, epochs=20, debug=True)
+    if retrain:
+        date = "2026-06-16"
+        extra_info = "epoch_3_dict"
+        run_name = f"{date}_{experiment_name}_{data_info}"
+        weights = torch.load(osp.join(CONFIG.model, run_name, f"{run_name}_{extra_info}.pt"), weights_only=True)
+        model.load_state_dict(weights["model_state_dict"], strict=False)
+        start_epoch = weights["epoch"]
+    else:
+        start_epoch = 0
+
+    train_model(model, data_info, experiment_name, CONFIG, epochs=20, debug=True, start_epoch=start_epoch+1)

ticllearning/cclinking/train.py
@@ -1,6 +1,6 @@
-import os.path as osp
 import os
 from datetime import datetime
+import os.path as osp
 
 import torch
 from torch.optim.lr_scheduler import CosineAnnealingLR
@@ -26,11 +26,11 @@ def train_epoch(epoch, model, data, loss_obj, optimizer, weighted=True):
         # reset optimizer and enable training mode
         optimizer.zero_grad(set_to_none=True)
 
+        weights = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
         z = model(sample.x, sample.A, sample.ranks, sample.num_cells)
         z = z[:-sample.num_cells[2]]
         
         # rescale weights to interval [0, 1]
-        weights = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
         weights /= 300
         weights = torch.clamp(weights, 0.0, 1.0)
         weights = weights
@@ -68,6 +68,8 @@ def test_epoch(epoch, model, data, loss_obj, config, weighted=True, threshold=0.
         stats = torch.zeros(4, device=config.device)
             
         for sample in tqdm(data, desc=f"Test Epoch {epoch}"):
+            weights = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
+
             z = model(sample.x, sample.A, sample.ranks, sample.num_cells)
             z = z[:-sample.num_cells[2]]
             
@@ -76,9 +78,6 @@ def test_epoch(epoch, model, data, loss_obj, config, weighted=True, threshold=0.
             y_pred = (z > threshold).squeeze()
             y_true = (sample.y > 0).squeeze()
             
-            # rescale weights to interval [0, 1]
-            weights = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
-
             stats[0] += torch.sum(weights * (y_true & y_pred)).item()
             stats[1] += torch.sum(weights * (~y_true & y_pred)).item()
             stats[2] += torch.sum(weights * (y_true & ~y_pred)).item()
@@ -104,6 +103,9 @@ def validate_epoch(epoch, model, data, loss_obj, config, weighted=True, threshol
         pred, ys, weights, ranks = [], [], [], []
             
         for sample in tqdm(data, desc=f"Validation Epoch {epoch}"):
+            weight = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
+            weights += weight.tolist()
+
             z = model(sample.x, sample.A, sample.ranks, sample.num_cells)
             z = z[:-sample.num_cells[2]]
             
@@ -112,8 +114,7 @@ def validate_epoch(epoch, model, data, loss_obj, config, weighted=True, threshol
             ranks += sample.ranks[:-(sample.num_cells[2]+sample.num_cells[3])].squeeze(-1).tolist()
 
             # rescale weights to interval [0, 1]
-            weight = torch.cat([sample.x[i][:, 3] for i in range(2)]).clone().detach()
-            weights += weight.tolist()
+
             weight /= 300
             weight = torch.clamp(weight, 0.0, 1.0)
             weight = weight.detach()
@@ -168,7 +169,7 @@ def train_model(model, dataset, experiment_name, config, start_epoch=0, epochs=1
 
         print("Fast statistic on model threshold:")
         print_acc_scores_from_precalc(*stats)
-        
+
         if (debug or ((epoch) % 10 == 0 and epoch != 0)):
             print("Store Diagrams")
 

ticllearning/utils/plot_results.py
@@ -66,11 +66,14 @@ def plot_binned_validation_results(pred, y, weights, bins, thres=0.65, output_fo
         n_classes = torch.unique(y_disc_bin).numel()
         y_disc_bin = y_disc_bin.numpy()
         if in_bin.sum() > 100 and n_classes == 2:
-            acc = accuracy_score(y_disc_bin, pred_disc_bin)
-            prec = precision_score(y_disc_bin, pred_disc_bin)
-            rec = recall_score(y_disc_bin, pred_disc_bin)
-            bin_vals.append((float(bin_edges[i]), float(bin_edges[i+1]), acc, prec, rec))
-            plot_validation_results(pred[in_bin], y[in_bin], thres, weight=weights[in_bin], file_suffix=f"{file_suffix}_bin_{type_bins}_{float(bin_edges[i])}_{float(bin_edges[i+1])}", output_folder=output_folder)
+            try:
+                acc = accuracy_score(y_disc_bin, pred_disc_bin)
+                prec = precision_score(y_disc_bin, pred_disc_bin)
+                rec = recall_score(y_disc_bin, pred_disc_bin)
+                bin_vals.append((float(bin_edges[i]), float(bin_edges[i+1]), acc, prec, rec))
+                plot_validation_results(pred[in_bin], y[in_bin], thres, weight=weights[in_bin], file_suffix=f"{file_suffix}_bin_{type_bins}_{float(bin_edges[i])}_{float(bin_edges[i+1])}", output_folder=output_folder)
+            except:
+                pass
 
     for low, high, acc, prec, rec in bin_vals:
         if acc != None:
@@ -214,6 +217,10 @@ def print_binned_acc_scores(pred, y, weights, thres=0.65):
 
 def plot_roc_curve(pred, y, ax, weight=None):
     y_discrete = (y > 0).astype(int)
+    n_classes = np.unique(y_discrete).shape[0]
+    if n_classes == 1:
+        return
+
     fpr, tpr, _ = roc_curve(y_discrete, pred, sample_weight=weight)
 
     fpr = np.round(fpr, decimals=8)